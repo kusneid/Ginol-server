fd9b576a670d719a892501dace6cf950fcc0690f
kusneid <kusneid@gmail.com>
2024-11-10 01:50:40 +0300

    [add] Login handler added

--- main.go
+++ main.go
@@ -2,28 +2,35 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"gorm.io/gorm"
+	"github.com/joho/godotenv"
+	"github.com/kusneid/Ginol-server/src"
+	"github.com/kusneid/Ginol/backend/user"
 )
 
 func main() {
 	r := gin.Default()
 
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	r.POST("/api/loginServerHandler", func(c *gin.Context) {
-		type Credentials struct {
-			gorm.Model
-			Username string `gorm:"username" json:"username"`
-			Password string `gorm:"password" json:"password"`
-		}
-		var creds Credentials
+
+		var creds user.Credentials
 		if err := c.ShouldBindJSON(&creds); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid data"})
 			return
 		}
 		fmt.Print(creds.Username, creds.Password)
+		if err := src.LoginCheck(&creds); err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authentication failed", "detail": err.Error()})
+			return
+		}
 	})
 
-	r.Run(":8081")
+	r.Run(":2737")
 }
--- src/loginCheck.go
+++ src/loginCheck.go
@@ -1,1 +1,42 @@
 package src
+
+import (
+	"log"
+	"os"
+
+	"github.com/kusneid/Ginol/backend/user"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func VerifyPassword(c *user.Credentials, providedPassword string) error {
+	return bcrypt.CompareHashAndPassword([]byte(c.Password), []byte(providedPassword))
+}
+
+var db *gorm.DB
+
+func InitDB() {
+	dsn := os.Getenv("DATABASE_DATA")
+	var err error
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Panic("failed to connect database", err)
+	}
+}
+
+func LoginCheck(c *user.Credentials) error {
+	InitDB()
+	var creds user.Credentials
+
+	if err := db.Where("username = ?", c.Username).First(&creds).Error; err != nil {
+		return err
+	}
+
+	if err := VerifyPassword(&creds, c.Password); err == nil {
+		return err
+	}
+
+	return nil
+
+}
